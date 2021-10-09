082609b4d9483f5655947a5a67717277b1d22f8c
Hikaru Nonose <52564646+TSUKASA2001@users.noreply.github.com>
2021-10-10 08:47:48 +0900

    Update FrequentItemSet.java

--- FrequentItemSet.java
+++ FrequentItemSet.java
@@ -1,37 +1,31 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class FrequentItemSet {
 	public static int length = 4;// 商品番号の最大値
+	
+	static BufferedWriter bout = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	static int returnCount(String fileName) throws IOException {// ファイルの行数 つまり客数を返す
-		int i = 0;
-		BufferedReader br = new BufferedReader(new FileReader(fileName));
-
-		while ((br.readLine()) != null) {
-			i++;
-		}
-		br.close();
-		return i;
-	}
-
-	static int[][] readData(String fileName, int n) throws IOException {// ファイルからメモリに格納するメソッド
+	static ArrayList<int[]>readData(String fileName) throws IOException {// ファイルからメモリに格納するメソッド
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
 
 		String buffer;
-		int[][] elements = new int[n][];
-		int i = 0;
+		ArrayList<int[]> elements = new ArrayList<int[]>();
 
 		while ((buffer = br.readLine()) != null) {
 			String[] s = buffer.split(" ");
-			elements[i] = new int[s.length];
+			int[] element = new int[s.length];
+			elements.add(element);
 			for (int j = 0; j < s.length; j++) {
-				elements[i][j] = Integer.parseInt(s[j]);
-				length = Math.max(length, elements[i][j]);
+				element[j] = Integer.parseInt(s[j]);
+				length = Math.max(length, element[j]);
 			}
-			i++;
+			Arrays.sort(element);			
 		}
 
 		br.close();
@@ -47,7 +41,7 @@ public class FrequentItemSet {
 	}
 	
 	static boolean contains(int[] elements, int x){
-		for(int i=0;i<elements.length;i++){
+		for(int i=0;i<elements.length && elements[i] <= x;i++){
 			if(elements[i] == x){
 				return true;
 			}
@@ -55,23 +49,6 @@ public class FrequentItemSet {
 		return false;
 	}
 	
-	static boolean discrimination2(int[] elements, int[] x) {// numはelementsの行数 elementsのnum行目にxが含まれていればtrue
-		//long start1=System.currentTimeMillis();														// 含まれていなければfalse
-		int i, count = 0;
-
-		for (i = 0; i < x.length; i++) {
-			if (contains(elements,x[i])) {
-				count++;
-			}
-		}
-		//time1+=System.currentTimeMillis()-start1;
-		if (count == x.length) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
 	static int[] arrayAdd(int[] x, int a) {// 配列xにaを追加する
 		int[] newx = new int[x.length + 1];
 		System.arraycopy(x, 0, newx, 0, x.length);
@@ -80,37 +57,57 @@ public class FrequentItemSet {
 		return newx;
 	}
 
-	static void mine(int[][] elements, int[] x, int min) {
-		for (int i = tail(x) + 1; i <= length + 1; i++) {
-
-			int[] newx = arrayAdd(x, i);
-
-			int count = 0;
-
-			for (int j = 0; j < elements.length; j++) {
-				if (discrimination2(elements[j], newx)) {
-					count++;
-				}
+	static void mine(ArrayList<int[]> elements, int[] x, int min, int [] preCounts) {
+		
+		int tail=tail(x);
+		int [] counts=new int [length+2];
+		ArrayList<int[]> [] newElements=new ArrayList[length+2];
+		for(int i=0;i<length+1;i++) {
+			if( tail<i && (preCounts==null ||preCounts[i]>=min))
+				newElements[i]=new ArrayList<int[]>();
+		}
+		for (int j = 0; j < elements.size(); j++) {
+			int[] transaction = elements.get(j);
+			for(int k=transaction.length-1;k>=0;k--) {
+				if(tail<transaction[k] && (preCounts==null ||preCounts[transaction[k]]>=min)) {
+					counts[transaction[k]]++;
+					newElements[transaction[k]].add(transaction);
+				}else
+					break;
 			}
-			if (count >= min) {// 閾値を超えているとき
-				System.out.println(Arrays.toString(newx) + (System.currentTimeMillis()-start));
-				mine(elements, newx, min);
+		}			
+		
+		for (int i = tail + 1; i <= length + 1; i++) {
+
+			if (counts[i] >= min) {// 閾値を超えているとき
+				int[] newx = arrayAdd(x, i);
+				try {
+					bout.write(Arrays.toString(newx));
+					bout.newLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				mine(newElements[i], newx, min, counts);
 			}
 		}
 	}
 	
-	static long start;
 
 	public static void main(String[] args) throws IOException {
 		
-		start=System.currentTimeMillis();
+		long start=System.currentTimeMillis();
 
 		String fileName="T10I4D100K.dat";
-		int min = 1000;
+		int min = 2;
 
-		int[][] elements = readData(fileName, returnCount(fileName));
+		ArrayList<int[]> elements = readData(fileName);
 		int x[] = new int[0];
 
-		mine(elements, x, min);// 閾値の設定
+		mine(elements, x, min, null);// 閾値の設定
+		
+		bout.close();
+
+		long end=System.currentTimeMillis();
+		System.err.println((end-start)+" msec");
 	}
 }
